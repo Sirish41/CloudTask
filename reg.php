c2eef8f2e4086e0eac7cd5b19f549bbf859b865f
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2023-11-02 21:21:47 +0530

    Create reg.php

---
+++ reg.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+
+	<?php echo "Welcome master ", $_POST['N1'], $_POST['N2'], $_POST['N3'];
+	?>
+	</br> 
+	<?php
+	      echo "Thank you for choosing ", $_POST['dropcourse'], " department\n";
+        echo "For further correspondence, you will be reached at; Phone: ", $_POST['Ph'],
+          "\nEmail: ", $_POST['mail'], "\nAddress: ", $_POST['addr'];
+	?>
+
+</body>
+
+</html>
