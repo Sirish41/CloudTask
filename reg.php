ab099fef38375d9c57bf945ce14c43469a5a6938
Sirish41 <86859551+Sirish41@users.noreply.github.com>
2023-11-02 21:25:58 +0530

    Update reg.php

--- reg.php
+++ reg.php
@@ -2,17 +2,9 @@
 
 <body>
 
-	<?php echo "Welcome master ", $_POST['N1'], $_POST['N2'], $_POST['N3'];
+	<?php echo "Welcome master ", $_POST['N1'];
 	?>
-	</br> 
-	<?php
-	      echo "Thank you for choosing ", $_POST['dropcourse'], " department\n";
-        echo "For further correspondence, you will be reached at; Phone: ", $_POST['Ph'];
 	</br>
-        echo "Email: ", $_POST['mail'];
-	</br>
-	echo "Address: ", $_POST['addr'];
-	?>
 
 </body>
 
